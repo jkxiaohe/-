92c5b39f30229af9cdfc386901b54eeaf46753a7
hexiaozhu <hexiaozhu@hotmail.com>
2018-05-15 14:28:58 +0800

    毕业设计5月15号运行代码

---
+++ src/main/java/com/jinke/dao/MessageDAO.java
@@ -0,0 +1,41 @@
+package com.jinke.dao;
+
+import com.jinke.model.Message;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+
+@Mapper
+public interface MessageDAO {
+    String TABLE_NAME = " message ";
+    String INSERT_FIELDS = " from_id, to_id, content, has_read, conversation_id, created_date ";
+    String SELECT_FIELDS = " id, " + INSERT_FIELDS;
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS,
+            ") values (#{fromId},#{toId},#{content},#{hasRead},#{conversationId},#{createdDate})"})
+    int addMessage(Message message);
+
+    @Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME,
+            " where conversation_id=#{conversationId} order by created_date desc limit #{offset}, #{limit}"})
+    List<Message> getConversationDetail(@Param("conversationId") String conversationId,
+                                        @Param("offset") int offset,
+                                        @Param("limit") int limit);
+
+/*    @Select({"select ", INSERT_FIELDS, " , count(id) as id from ( select * from ", TABLE_NAME,
+            " where from_id=#{userId} or to_id=#{userId} order by created_date desc) tt group by conversation_id order by created_date desc limit #{offset}, #{limit}"})
+    List<Message> getConversationList(@Param("userId") int userId,
+                                      @Param("offset") int offset,
+                                      @Param("limit") int limit);*/
+    @Select({"select min(from_id) as from_id,max(to_id) as to_id,min(content) as content,max(has_read) as has_read,min(created_date) as created_date,conversation_id,count(1) as id" +
+            " from message " +
+            " where from_id=#{userId} or to_id=#{userId}" +
+            " group by conversation_id" +
+            " limit #{offset},#{limit}"})
+    List<Message> getConversationList(@Param("userId") int userId,
+                                      @Param("offset") int offset,
+                                      @Param("limit") int limit);
+
+    @Select({"select count(id) from ", TABLE_NAME, " where has_read=0 and to_id=#{userId} and conversation_id=#{conversationId}"})
+    int getConversationUnreadCount(@Param("userId") int userId, @Param("conversationId") String conversationId);
+}
---
+++ src/test/java/com/jinke/WendaApplicationTests.java
@@ -0,0 +1,20 @@
+package com.jinke;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WendaApplication.class)
+@WebAppConfiguration
+public class WendaApplicationTests {
+
+	@Test
+	@Sql("/init-schema.sql")
+	public void contextLoads() {
+	}
+
+}
